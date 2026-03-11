058e33527a2a2c53b7a1ed33638783084b264f1f
oyn0622 <oh990508@gmail.com>
2026-03-11 15:22:19 +0900

    js css

---
+++ blocks/fiberoffer/fiberoffer.js
@@ -0,0 +1,12 @@
+export default function decorate(block) {
+  const offerText = block.children[0];
+  const offerCTA = block.children[1];
+  const offerImage = block.children[2];
+
+  offerText.id = 'offerText';
+  offerText.className = 'offerText';
+  offerCTA.id = 'offerCTA';
+  offerCTA.className = 'offerCTA';
+  offerImage.id = 'offerImage';
+  offerImage.className = 'offerImage';
+}
